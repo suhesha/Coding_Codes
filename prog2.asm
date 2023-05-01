6ff2b76c3a598aa545fe961b24e3ef9d03886acb
Suhesha <81362217+suhesha@users.noreply.github.com>
2023-05-01 17:56:35 +0530

    Update prog2.asm

--- prog2.asm
+++ prog2.asm
@@ -1,18 +1,15 @@
 ;Program title : ALP to chek even and odd
-
 section .data
 	even_msg db "Even Number ! "
 	len1 equ $-even_msg
 	odd_msg db "Odd Number ! "
 	len2 equ $-odd_msg
-	
 section .text
 	global _start 
     _start :
 	mov eax,15h
 	and eax,1
 	jz even
-	
    odd :mov edx,len2
 	mov ecx,odd_msg
 	mov ebx,1
